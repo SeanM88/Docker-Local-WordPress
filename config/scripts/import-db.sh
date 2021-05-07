93b72062c502909f844193cff7ec738e7d3bdc6a
Sean Megan <sean@seanmegan.com>
2021-05-06 21:59:10 -0400

    Fix docker exec db import command to use right filepath

--- config/scripts/import-db.sh
+++ config/scripts/import-db.sh
@@ -11,7 +11,7 @@ DB_ROOT_PASSWORD=$(echo "$DB_ROOT_PASSWORD")
 # try to import database file if it exists using docker exec command (https://docs.docker.com/engine/reference/commandline/exec/)
 if [ -f "$DB_PATH/$DB_FILE" ]; then
   echo "Found ${DB_FILE} file, importing now..."
-  docker exec -i "${COMPOSE_PROJECT_NAME}_mysql_1" sh -c 'exec mysql -uroot -p"${DB_ROOT_PASSWORD}"' < $(echo "$DB_FILE")
+  docker exec -i "${COMPOSE_PROJECT_NAME}_mysql_1" sh -c 'exec mysql -uroot -p"${DB_ROOT_PASSWORD}"' < $(echo "$DB_PATH/$DB_FILE")
 else
   echo "ERROR: Unable to find '${DB_FILE}' file in project's '/config/database' directory!"
   echo "To import a database using this script, place '${DB_FILE}' file in project's '/config/database' directory, then try again."
