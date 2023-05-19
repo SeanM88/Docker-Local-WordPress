77c2c7443538d44e910bc33205a28cbef6a224d1
Sean Megan <SeanM88@users.noreply.github.com>
2023-05-19 18:25:15 -0400

    Update db import script to reference correct container name

--- config/scripts/import-db.sh
+++ config/scripts/import-db.sh
@@ -12,7 +12,7 @@ DB_PASSWORD=$(echo "$DB_PASSWORD")
 # try to import database file if it exists using docker exec command (https://docs.docker.com/engine/reference/commandline/exec/)
 if [ -f "$DB_PATH/$DB_FILE" ]; then
   echo "Found ${DB_FILE} file, importing now..."
-  docker exec -i "${COMPOSE_PROJECT_NAME}_mysql_1" sh -c "exec mysql -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME}" < $(echo "$DB_PATH/$DB_FILE")
+  docker exec -i "${COMPOSE_PROJECT_NAME}-mysql-1" sh -c "exec mysql -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME}" < $(echo "$DB_PATH/$DB_FILE")
 else
   echo "ERROR: Unable to find '${DB_FILE}' file in project's '/config/database' directory!"
   echo "To import a database using this script, place '${DB_FILE}' file in project's '/config/database' directory, then try again."
